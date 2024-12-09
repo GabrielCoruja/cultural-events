9dc803b15807898a3fdc58551cc8c59cbe464cab
Gabriel Dalseco <gabridal96@gmail.com>
2024-12-08 23:38:07 -0300

    feat(test): base para a construcao dos testes

---
+++ culturalevents-backend/src/test/java/com/ona/culturalevents/CulturalEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ona.culturalevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CulturalEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
