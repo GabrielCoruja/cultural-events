d064c05cdc150db1d8763d1f56d1bd41daa52adb
Gabriel Dalseco <gabridal96@gmail.com>
2024-12-08 23:37:11 -0300

    feat: adicao de services para a aplicacao

---
+++ culturalevents-backend/src/main/java/com/ona/culturalevents/service/CategoryService.java
@@ -0,0 +1,60 @@
+package com.ona.culturalevents.service;
+
+import com.ona.culturalevents.entity.Category;
+import com.ona.culturalevents.exception.badrequest.DuplicateEntryException;
+import com.ona.culturalevents.exception.notfound.CategoryNotFoundExpection;
+import com.ona.culturalevents.repository.CategoryRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+
+  private final CategoryRepository categoryRepository;
+
+  @Autowired
+  public CategoryService(CategoryRepository categoryRepository) {
+    this.categoryRepository = categoryRepository;
+  }
+
+  public List<Category> findAll() {
+    return categoryRepository.findAll();
+  }
+
+  public Category findById(long categoryId) throws CategoryNotFoundExpection {
+    return categoryRepository.findById(categoryId)
+        .orElseThrow(CategoryNotFoundExpection::new);
+  }
+
+  private void validateUniqueName(String name) throws DuplicateEntryException {
+    if (categoryRepository.findByName(name).isPresent()) {
+      throw new DuplicateEntryException("Category name must be unique.");
+    }
+  }
+
+  public Category create(Category category) throws DuplicateEntryException {
+    validateUniqueName(category.getName());
+
+    return categoryRepository.save(category);
+  }
+
+  public Category update(Category category, long categoryId)
+      throws CategoryNotFoundExpection, DuplicateEntryException {
+    Category findCategory = findById(categoryId);
+
+    validateUniqueName(category.getName());
+
+    findCategory.setName(category.getName());
+
+    return categoryRepository.save(findCategory);
+  }
+
+  public void remove(long categoryId) throws CategoryNotFoundExpection {
+    if (categoryRepository.existsById(categoryId)) {
+      throw new CategoryNotFoundExpection();
+    }
+
+    categoryRepository.deleteById(categoryId);
+  }
+}
---
+++ culturalevents-backend/src/main/java/com/ona/culturalevents/service/EventService.java
@@ -0,0 +1,66 @@
+package com.ona.culturalevents.service;
+
+import com.ona.culturalevents.entity.Category;
+import com.ona.culturalevents.entity.Event;
+import com.ona.culturalevents.exception.notfound.CategoryNotFoundExpection;
+import com.ona.culturalevents.exception.notfound.EventNotFoundExpection;
+import com.ona.culturalevents.repository.CategoryRepository;
+import com.ona.culturalevents.repository.EventRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EventService {
+
+  private final EventRepository eventRepository;
+  private final CategoryRepository categoryRepository;
+
+  @Autowired
+  public EventService(EventRepository eventRepository, CategoryRepository categoryRepository) {
+    this.eventRepository = eventRepository;
+    this.categoryRepository = categoryRepository;
+  }
+
+  public List<Event> findAll() {
+    return eventRepository.findAll();
+  }
+
+  public Event findById(long eventId) throws EventNotFoundExpection {
+    return eventRepository.findById(eventId)
+        .orElseThrow(EventNotFoundExpection::new);
+  }
+
+  public Event create(Event event, List<Long> categoryIds) throws CategoryNotFoundExpection {
+    List<Category> findCategories = categoryRepository.findAllById(categoryIds);
+
+    if (findCategories.size() != categoryIds.size()) {
+      throw new CategoryNotFoundExpection();
+    }
+
+    event.setCategories(findCategories);
+
+    return eventRepository.save(event);
+  }
+
+  public Event update(Event event, long eventId)
+      throws EventNotFoundExpection {
+    Event findEvent = findById(eventId);
+
+    findEvent.setName(event.getName());
+    findEvent.setDescription(event.getDescription());
+    findEvent.setEventDate(event.getEventDate());
+    findEvent.setLocation(event.getLocation());
+    findEvent.setCategories(event.getCategories());
+
+    return eventRepository.save(findEvent);
+  }
+
+  public void remove(long categoryId) throws EventNotFoundExpection {
+    if (!eventRepository.existsById(categoryId)) {
+      throw new EventNotFoundExpection();
+    }
+
+    eventRepository.deleteById(categoryId);
+  }
+}
